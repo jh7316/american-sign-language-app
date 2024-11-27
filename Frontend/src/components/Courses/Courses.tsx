9470ab93a8c45ae1dc544bd5a4da774bc6e3d55b
jh7316 <jh7316@nyu.edu>
2024-11-26 20:20:36 -0500

    fix styling

--- Frontend/src/components/Courses/Courses.tsx
+++ Frontend/src/components/Courses/Courses.tsx
@@ -2,15 +2,19 @@ import styles from './Courses.module.scss'
 import Course from './Course/Course'
 import { COURSES } from '../../constants/constants'
 
-
 const Courses = ()=>{
+
+    const handleClick = ()=>{
+        window.location.href = '/courses/beginner'
+    }
+
 
     return <div className={styles.main}>
         <h2>Courses</h2>
         <div className={styles.courses}>
             {COURSES.map((item)=><Course key={item.title} course={item}/>)}
         </div>
-        <button>Start Now</button>
+        <button onClick={handleClick}>Start Now</button>
     </div>
 }
 
